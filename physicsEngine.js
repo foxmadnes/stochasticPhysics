965869da5fa944a1e3c904dca334b6eb9e6b753a
Andres Romero <andresr@andresr-Latitude-E6410.(none)>
2013-01-29 13:58:00 -0500

    Changes that fix EOF error, commit to show Yura

--- physicsEngine.js
+++ physicsEngine.js
@@ -1,15 +1,10 @@
 // Javascript for the Stochastic 2D physics engine built by Andres Romero. This js is based on the CRP demo, credit to Ardavan Saeedi.
 
 
-// Global variable to save the all the current path predictions
-currentPathPredictions =  new Object();
-
-//Global list to store the next predicted x and y for each point
-currentCoordPredictions = new Object();
 
 //Global Timer
 time = 0;
-var timerid;
+timerid = 0;
 
 function InitializeDemo() {
   ripl = new ripl(); // Create a RIPL client object to communicate with the engine.
@@ -22,40 +17,38 @@ function InitializeDemo() {
   // Define the generic model:
   predicted_coords = new Array();
   
-  ripl.assume('initial-pos-x',parse('(mem (normal 0.0 1.0))'));
-  ripl.assume('initial-vel-x',parse('(mem (normal 0.0 1.0))'));
-  ripl.assume('force-x',parse('(mem (normal 0.0 1.0))'));
-  predicted_coords[0] = ripl.assume('pos-x',parse('(mem (lambda (time) (if (= time c[0]) (initial-pos-x) (+ pos-x (dec time))(initial-vel-x)(force-x)(normal 0.0 0.01))'));
+  ripl.assume('initial-pos-x','(mem (normal 0.0 1.0))');
+  ripl.assume('initial-vel-x','(mem (normal 0.0 1.0))');
+  ripl.assume('force-x','(mem (normal 0.0 1.0))');
+  predicted_coords[0] = ripl.assume('pos-x','(mem (lambda (time) (if (= time c[0]) (initial-pos-x) (+ (pos-x (dec time))(initial-vel-x)(force-x)(normal 0.0 0.01)))))');
   
-  ripl.assume('initial-pos-y',parse('(mem (normal 0.0 1.0))'));
-  ripl.assume('initial-vel-y',parse('(mem (normal 0.0 1.0))'));
-  ripl.assume('force-y',parse('(mem (+ 9.8 (normal 0.0 1.0)))'));
-  predicted_coords[1] = ripl.assume('pos-y',parse('(mem (lambda (time) (if (= time c[0]) (initial-pos-y) (+ pos-y (dec time))(initial-vel-y)(force-y)(normal 0.0 0.01))'));
+  ripl.assume('initial-pos-y','(mem (normal 0.0 1.0))');
+  ripl.assume('initial-vel-y','(mem (normal 0.0 1.0))');
+  ripl.assume('force-y','(mem (+ 9.8 (normal 0.0 1.0)))');
+  predicted_coords[1] = ripl.assume('pos-y','(mem (lambda (time) (if (= time c[0]) (initial-pos-y) (+ (pos-y (dec time))(initial-vel-y)(force-y)(normal 0.0 0.01)))))');
   ripl.start_cont_infer(1); // Start the continuous ("infinite") inference.
 
-  // Prepare the canvas in your browser.
-  var canvas = d3.select('#graphics_div').append("canvas").attr("width",420).attr("height",420);
-  
   all_points = new Object(); // Init a JavaScript dictionary to save current points.
   next_point_unique_id = 0;
   
-  for (i in range(0,100)) {
+  for (var i=0; i < 100; i++ ) {
     ripl.predict('pos-x','c[' + i + ']');
   }
     
-  for (i in range(0,100)) {
+  for (var i=0; i < 100; i++ ) {
     ripl.predict('pos-y', 'c[' + i + ']');
   }
 
     
-  ripl.predict('initial-velocity-x');
-  ripl.predict('initial-velocity-y');
+  ripl.predict('initial-vel-x');
+  ripl.predict('initial-vel-y');
   ripl.predict('force-x');
   ripl.predict('force-y');
     
   
-  
-  background.click(function(background_event) {
+  // Prepare the canvas in your browser.
+  var canvas = d3.select('#graphics_div').append("canvas").attr("width",420).attr("height",420).style("stroke","gray")
+  .on("click",function(d) {
     if (time > 100) {
       return;
     }
@@ -66,8 +59,7 @@ function InitializeDemo() {
     var point = new Object();
     
     // Coordinates in the browser window:
-    point.html_x = background_event.pageX - $('#graphics_div').offset().left;
-    point.html_y = background_event.pageY - $('#graphics_div').offset().top;
+    [point.html_x,point.html_y] = d3.mouse(canvas);
     
     // Create and pretty a circle on the plot.
     canvas.append("circle").style("stroke","gray").style("fill","grey")
@@ -112,7 +104,7 @@ function requestPath() {
   current_obs_noise = ripl.report_value(physic_noise_directive_id)['val'];
   current_phys_noise = ripl.report_value(obs_noise_directive_id)['val'];
   var points = new Array();
-  for (i in range(0,100)) {
+  for (var i=0; i < 100; i++ ) {
     x = ripl.report_value(predicted_coords[0])['val'];
     y = ripl.report_value(predicted_coords[1])['val'];
     points[i] = [x,y];
@@ -124,9 +116,5 @@ function requestPath() {
 }
 
 function drawPath(points) {
-  canvas.append("path").data([points]).attr('d',line).style("stoke","#000000")
-  canvas.append("circle").attr("cx",points[0][0]).attr("cy",points[0][1]);
-  for (point in points) {
-    
-  }
+  canvas.append("path").data([points]).attr('d',line).style("stoke","#000000");
 }
